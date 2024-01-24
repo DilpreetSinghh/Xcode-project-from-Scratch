c0468a29b1744851db1af8e670364e2ce1bebae4
Dilpreet Singh <dilpreetsinghhere@gmail.com>
2024-01-24 14:53:25 +0530

    Framework

--- Apple-Frameworks/Apple-Frameworks/FrameworkGridView.swift
+++ Apple-Frameworks/Apple-Frameworks/FrameworkGridView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  FrameworkGridView.swift
 //  Apple-Frameworks
 //
 //  Created by Dilpreet Singh on 21/01/24.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct FrameworkGridView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    FrameworkGridView()
 }
